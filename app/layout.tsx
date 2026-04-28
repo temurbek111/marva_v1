fb094654fe5ccd677e2a89594739666867ec966b
temur <temurbeksamadov47@gmail.com>
2026-04-28 11:44:30 +0500

    Load Telegram WebApp SDK

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import Script from "next/script";
 import { TelegramInit } from "@/components/layout/TelegramInit";
-import "./globals.css";
 import { LangProvider } from "@/components/common/LangProvider";
+import "./globals.css";
+
 export const metadata: Metadata = {
   title: "MARVA Dental shop",
   description: "Dental market mini app",
@@ -16,7 +17,15 @@ export default function RootLayout({
   return (
     <html lang="uz">
       <body>
-        <LangProvider>{children}</LangProvider>
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
+
+        <LangProvider>
+          <TelegramInit />
+          {children}
+        </LangProvider>
       </body>
     </html>
   );
